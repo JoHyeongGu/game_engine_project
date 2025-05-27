8b92d9055f448ef2f28571e22eafaae9072dc0ec
JoHyeongGu <whgudrn123@naver.com>
2025-05-27 14:32:19 +0900

    Update Unit_3.cs

--- Assets/Scripts/Defense/Unit_3.cs
+++ Assets/Scripts/Defense/Unit_3.cs
@@ -9,25 +9,42 @@ public class Unit_3 : Unit
         isAttacking = true;
         while (true)
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(10);
             if (targetList.Count == 0) break;
-            GameObject firstEnemy = targetList[0];
-            if (firstEnemy == null)
+            GameObject _enemy = GetFullHpEnemy();
+            if (_enemy == null)
             {
-                targetList.Remove(targetList[0]);
+                targetList.Remove(_enemy);
                 continue;
             }
-            Enemy enemyClass = firstEnemy.GetComponent<Enemy>();
+            Enemy enemyClass = _enemy.GetComponent<Enemy>();
             if (enemyClass == null)
             {
-                targetList.Remove(targetList[0]);
+                targetList.Remove(_enemy);
                 continue;
             }
             enemyClass.hp -= 10;
             if (enemyClass.hp <= 0)
             {
-                targetList.Remove(targetList[0]);
+                targetList.Remove(_enemy);
             }
         }
+    }
+
+    private GameObject GetFullHpEnemy()
+    {
+        GameObject fullHpEnemy = null;
+        int hp = 0;
+        foreach (GameObject _enemy in targetList)
+        {
+            Enemy _class = _enemy.GetComponent<Enemy>();
+            if (_class.hp >= hp)
+            {
+                fullHpEnemy = _enemy;
+                hp = _class.hp;
+            }
+        }
+        Debug.Log($"{hp}");
+        return fullHpEnemy;
     }
 }
